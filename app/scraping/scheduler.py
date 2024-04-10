a0bc2d1ebbee22bd05a3e2e5473aaaf747d54d6e
Cuupa <simonthiel2@gmail.com>
2024-04-10 16:53:57 +0200

    fixed none-check

--- app/scraping/scheduler.py
+++ app/scraping/scheduler.py
@@ -18,7 +18,7 @@ def scrape_products():
             if name == "" or name is None:
                 continue
             store.add_point(product.id, name, price, currency, date_time)
-            if price < product.current_price and product.current_price is not None:
+            if product.current_price is not None and price < product.current_price:
                 notifications.notify(product, price)
         except Exception as error:
             print("An Error occured", error)
